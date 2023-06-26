cd20e844196e9223e6de06d52d2c527bc370ab5d
José Manuel Migoya Bussolotti <52668755+migmanu@users.noreply.github.com>
2023-06-26 16:01:17 +0200

    Define l_list

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,28 @@
 /*   By: migmanu </var/spool/mail/migmanu>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 15:38:02 by migmanu           #+#    #+#             */
-/*   Updated: 2023/06/24 15:38:07 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/06/26 15:55:40 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+typedef struct s_list
+{
+	char		*str_buff;
+	struct s_list	*next;
+}			l_list;
+
 char *get_next_line(int fd)
+
+#endif
